ac753e57e71256ebd7c26b3a11e4f509bbf288fd
smkrishna27 <smkrishna27@gmail.com>
2026-06-17 21:44:32 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Map<Integer,Integer> map=new HashMap();
+
+        for(int num: nums){
+            if(map.containsKey(num)){
+                 return true;
+            }
+            map.put(num,num);
+    
+
+
+        }
+        return false;
+        
+    }
+}
