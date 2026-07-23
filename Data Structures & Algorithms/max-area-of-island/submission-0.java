673f0c42e749ad28493c97386eebc6ad74c0e108
smkrishna27 <smkrishna27@gmail.com>
2026-07-23 18:31:46 +0530

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+     if (grid == null || grid.length == 0) return 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int max = 0;
+        int size = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    // size = dfs(grid, i, j);
+                    size = bfs(grid, i, j);
+                    max = Math.max(max, size);
+
+                }
+            }
+        }
+        return max;
+    }
+
+    public  int bfs(int[][] grid, int i, int j) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int size = 1;
+        int max = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{i, j});
+        grid[i][j] = 0; //visited
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            for (int[] d : directions) {
+                int x = cell[0] + d[0];
+                int y = cell[1] + d[1];
+                if (x >= 0 && x < rows && y >= 0 && y < cols && grid[x][y] == 1) {
+                    grid[x][y] = 0;
+                    size++;
+                    queue.offer(new int[]{x, y});
+
+                }
+
+            }
+        }
+
+        return size;
+
+
+    }
+}
