687f1f203e9c52b3442b647525a81a0696678b3e
smkrishna27 <smkrishna27@gmail.com>
2026-07-25 17:27:24 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rob(int[] nums) {
+       if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+
+        int case1 = robLiner(nums, 0, nums.length - 2);
+        int case2 = robLiner(nums, 1, nums.length - 1);
+        return Math.max(case1, case2);
+    }
+
+    private static int robLiner(int[] nums, int start, int end) {
+        int prev2 = 0, prev1 = 0;
+        for (int i = start; i <= end; i++) {
+            int cur = Math.max(prev1 , prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = cur;
+
+        }
+        return prev1;
+
+    }
+}
